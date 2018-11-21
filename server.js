1727c8e47fb90ee0ad45fd878e5149a95e165841
jacekjp <jacekjp@gmail.com>
2018-11-21 11:28:56 +0100

    add an event listener for the data event

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var url = require("url");
 
 function start(route, handle){
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
         request.setEncoding('utf8');
+
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+
+            postDataChunk + "'.");
+        });
 
         route(handle, pathname, response);
     }
